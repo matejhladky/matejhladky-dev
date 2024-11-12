5b0948f72ffcd6daaa33bd19abf0a5c886bb2376
mathladky <mat.hladky@gmail.com>
2024-11-12 13:46:12 +0100

    added script for component loading and first component

---
+++ scripts/componentLoader.js
@@ -0,0 +1,21 @@
+async function loadComponent(selector, filePath) {
+  const element = document.querySelector(selector);
+  if (element) {
+    const response = await fetch(filePath);
+    const html = await response.text();
+    element.innerHTML = html;
+
+    setActiveLink();
+  }
+}
+
+function setActiveLink() {
+  const currentPage = window.location.pathname.split("/").pop().split(".")[0];
+  const activeLink = document.querySelector(`.nav-link[data-page="${currentPage}"]`);
+  if (activeLink) {
+    activeLink.classList.add("active");
+  }
+}
+
+loadComponent("#footer", "/components/footer.html");
+loadComponent("#header", "/components/header.html");
