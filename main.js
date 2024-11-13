588422c2d053e544566b42716964c32f9e95ef25
mathladky <mat.hladky@gmail.com>
2024-11-14 00:11:03 +0100

    resolved production issues with static files

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+import { loadComponent } from './scripts/componentLoader.js';
+
+loadComponent('#header', './components/header.html');
+loadComponent('#footer', './components/footer.html');
+loadComponent('#loader', './components/loader.html');
