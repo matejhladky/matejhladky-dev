0da215ed7d0c236359c43a1b28094d8ecd76767e
mathladky <mat.hladky@gmail.com>
2024-11-24 23:37:33 +0100

    added medium article loading

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 import { loadComponent } from './scripts/componentLoader.js';
+import { formatDate } from './scripts/utils.js';
 
 loadComponent('#header', './components/header.html');
 loadComponent('#footer', './components/footer.html');
